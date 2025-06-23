612a250688c2f0c1f8a066afe24df6aebe32f9f4
yamuna aggala <aggalayamuna46@gmail.com>
2025-06-23 21:43:26 +0530

    about page

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from '../../CommonComponents/About/Aboutus';
+
+
+const Aboutpage = () => {
+    return(
+        <div>
+            <About/>
+            
+        </div>
+    )
+}
+export default Aboutpage;
