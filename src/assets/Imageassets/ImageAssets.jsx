ba8d8b4c1c2131ba445e0ba870a6f0d32ab902a6
chaitu5597 <chaitanyakumar5597@gmail.com>
2025-06-25 17:11:13 +0530

    images folder

---
+++ src/assets/Imageassets/ImageAssets.jsx
@@ -0,0 +1,9 @@
+
+export const ImageAssets = {
+    aboutus: require('../images/about.jpg'),
+    cat1: require('../images/cat-1.jpg'),
+    cat2: require('../images/cat-2.jpg'),
+    cat3: require('../images/cat-3.jpg'),
+    cat4: require('../images/cat-4.jpg'),
+    
+};
