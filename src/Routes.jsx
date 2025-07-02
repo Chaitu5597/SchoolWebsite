1da42c3c41d5248869ce5dffead9e9652cee476d
mamathasherla <mammusherla13@gmail.com>
2025-07-02 22:30:02 +0530

    course blog contact

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,66 +1,43 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layoutpage from '../src/Pages/Layoutpage';
 import Home from './Pages/Home/Home';
-<<<<<<< HEAD
-import About from './CommonComponents/About/Aboutus';
-import  Cources from './CommonComponents/Cources/Cources';
-import Courcesblog from './Pages/Cources/Courcesblog';
-import { ContactPage } from '@mui/icons-material';
-import Contactus from './CommonComponents/Contact/Contactus';
-=======
 import About from './Pages/About/About';
-import Cources from './CommonComponents/Cources/Cources';
-import Contactus from './CommonComponents/Contact/Contactus';
 import Teacherpage from './Pages/Teacher/Teacher';
-
-
->>>>>>> fd18e8a880e77ff00dd9f7fb1e69c7d6cb2020f9
+import Cources from './CommonComponents/Cources/Cources';
+import Courcesblog from './Pages/Cources/Courcesblog';
+import Contactus from './CommonComponents/Contact/Contactus';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layoutpage />,
     children: [
-      { 
+      {
         index: true,
-         element: <Home />
-         },
-        {
-         path: 'about',
-         element: <About/> 
-         },
-
-          {
-         path: 'teacher',
-         element: <Teacherpage /> 
-         },
-   
-
-         {
-         path: 'course',
-         element: <Cources/> 
-         },
-<<<<<<< HEAD
-         {
-          path: 'courcesblog',
-          element: <Courcesblog/>
-         },
-          {
-          path: 'contact',
-          element: <Contactus/>
-         }
-=======
->>>>>>> fd18e8a880e77ff00dd9f7fb1e69c7d6cb2020f9
-
-          
-           {
-         path: 'Contactus',
-         element: <Contactus/> 
-         }            
-
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'teacher',
+        element: <Teacherpage />,
+      },
+      {
+        path: 'course',
+        element: <Cources />,
+      },
+      {
+        path: 'courcesblog',
+        element: <Courcesblog />,
+      },
+      {
+        path: 'contact',
+        element: <Contactus />,
+      },
     ],
   },
-  
 ]);
 
 export default router;
